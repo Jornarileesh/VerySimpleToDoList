87ec8dada4a2ab2974403ed9091abb8f27a333fa
Jornarileesh <t.ciarachowicz@gmail.com>
2017-08-04 00:09:51 +0200

    przygotowania pod todolistę 2.0

--- VerySimpleToDoList/Program.cs
+++ VerySimpleToDoList/Program.cs
@@ -10,7 +10,8 @@ namespace VerySimpleToDoList
     {
         static void Main(string[] args)
         {
-            List<string> todolist = new List<string>();
+            Dictionary<int, string> todoList = new Dictionary<int, string>();
+
             Console.WriteLine("Witaj w programie VerySimpleToDoList.");
             Console.WriteLine("Aby dodać tekst do listy użyj prefixu ze znakiem plus, np. +Kup mleko.");
             Console.WriteLine("Aby usunąć tekst z listy użyj prefixu ze znakiem minus, np. -Kup mleko.");
@@ -26,32 +27,49 @@ namespace VerySimpleToDoList
                 Console.Clear();
                 Console.WriteLine("//----- Twoja TODO lista -----//");
 
+                int Id = todoList.Count();
+
+                // poniżej poprawić
+                if (Id > 0)
+                {
+                    int maxId = todoList.Keys.Max();
+                    Console.WriteLine(maxId);
+                }
+                else
+                {
+                    Console.WriteLine("brak");
+                }
+                // -----
+
                 if (b.Equals("+") || b.Equals("-") || b.Equals("!"))
                 {
                     switch (b)
                     {
                         case "+":
-                            todolist.Add(c);
+                            todoList.Add(maxId + 1, c);
                             break;
                         case "-":
-                            todolist.Remove(c);
+                            todoList.Remove(Int32.Parse(c));
                             break;
                         case "!":
-                            todolist.Clear();
+                            todoList.Clear();
                             break;
                     }
                 }
                 else Console.WriteLine("Musisz dodać prefix ze znakiem plus lub minus do słowa by je dodać lub usunąć. Wpisanie wykrzyknika wyczyści listę.");
 
-                todolist.ForEach(Console.WriteLine);
+                foreach (KeyValuePair<int, string> item in todoList)
+                {
+                    Console.WriteLine("{0}. {1}", item.Key, item.Value);
+                }
 
             } while (true);
 
-            // To Do
-            // 1. ustawić rozmiar okienka
-            // 2. Zapisywać nr zadania
-            // 3. Wyświetlać nr zadania
-            // 4. Usuwać wg numeru
-        }
-    }
+// To Do
+// 1. ustawić rozmiar okienka
+// 2. Zapisywać nr zadania
+// 3. Wyświetlać nr zadania
+// 4. Usuwać wg numeru
+}
+}
 }
